27db4dea5ee6d20364a3a64eb7a174bd9a073b04
JustRafi <116659811+JustRafi@users.noreply.github.com>
2025-08-24 03:05:09 +0200

    feat init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './layouts/default';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <div>
+        Init
+      </div>
+    </Layout>
+  );
+};
+
+export default App
---
+++ src/layouts/default.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen">
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
